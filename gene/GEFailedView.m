591d63d92f4112ec25ef2d2d599096bbcb576fc1
Chi-An Wang <mw10104587@gmail.com>
2012-12-20 05:47:55 +0800

    GESoundManager add function,  add highlight images

---
+++ gene/GEFailedView.m
@@ -0,0 +1,31 @@
+//
+//  GEFailedView.m
+//  gene
+//
+//  Created by Wang Chi-An on 12/12/20.
+//  Copyright (c) 2012年 An-Ruei-Che. All rights reserved.
+//
+
+#import "GEFailedView.h"
+
+@implementation GEFailedView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
